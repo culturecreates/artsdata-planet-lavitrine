385e0a6bf6bcc676d169ada3dd1c58ee51f6ac1a
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-12-11 11:51:38 -0500

    added org pipeline

--- src/lavitrine_organization_pipeline.rb
+++ src/lavitrine_organization_pipeline.rb
@@ -2,14 +2,22 @@
 
 require_relative './utils/artsdata'
 
-def LavitrinePipeline()
+def LavitrinePipeline(graph: "organizations")
   
   pipeline = ArtsdataPipeline.new
 
-    # puts "Downloading data..."
-    pipeline.load(sparql: "./sparql/load_artsdata_organizations.sparql", limit: 30)
-    pipeline.dump("../output/raw-organizations.json")
+  puts "Downloading data..."
+  pipeline.load(sparql: "./sparql/load_artsdata_organizations.sparql", limit: 30)
+  pipeline.dump("../output/raw-organizations.json")
 
+  puts "Starting transforms..."
+  pipeline.transform("./sparql/convert_url_literal_to_uri.sparql")
+
+  puts "Framing..."
+  pipeline.frame("../frame/lavitrine_organization_frame.jsonld")
+
+  puts "Saving JSON-LD..."
+  pipeline.dump("../output/#{graph.split("/").last}.json")
 end
 
 
