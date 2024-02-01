578aa6094e4b97e6dbbb9ce67475b07b0ed89d31
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-02-01 15:36:41 -0500

    batch all graphs

--- src/batch.rb
+++ src/batch.rb
@@ -2,10 +2,10 @@
 
 require_relative 'lavitrine_pipeline.rb'
 
-# LavitrinePipeline(graph: "http://kg.artsdata.ca/culture-creates/footlight/placedesarts-com")
-# LavitrinePipeline(graph: "http://kg.artsdata.ca/resource/dia-logGraphs") 
-# LavitrinePipeline(graph: "http://kg.artsdata.ca/culture-creates/footlight/hector-charland-com")
-# LavitrinePipeline(graph: "http://kg.artsdata.ca/culture-creates/footlight/theatredumarais-com")
-# LavitrinePipeline(graph: "http://kg.artsdata.ca/culture-creates/huginn/derived-grandtheatre-qc-ca")
+LavitrinePipeline(graph: "http://kg.artsdata.ca/culture-creates/footlight/placedesarts-com")
+LavitrinePipeline(graph: "http://kg.artsdata.ca/resource/dia-logGraphs") 
+LavitrinePipeline(graph: "http://kg.artsdata.ca/culture-creates/footlight/hector-charland-com")
+LavitrinePipeline(graph: "http://kg.artsdata.ca/culture-creates/footlight/theatredumarais-com")
+LavitrinePipeline(graph: "http://kg.artsdata.ca/culture-creates/huginn/derived-grandtheatre-qc-ca")
 LavitrinePipeline(graph: "http://kg.artsdata.ca/culture-creates/artsdata-planet-footlight/tout-culture-cms-events")
-# LavitrinePipeline(graph: "http://kg.artsdata.ca/culture-creates/artsdata-planet-footlight/signe-laval-cms-events")
+LavitrinePipeline(graph: "http://kg.artsdata.ca/culture-creates/artsdata-planet-footlight/signe-laval-cms-events")
