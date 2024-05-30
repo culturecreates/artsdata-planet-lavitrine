06b0bcc5343a6830d96253fe1ac89780bd95b033
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-05-29 22:41:33 -0400

    sparql tests

---
+++ src/tests/shacl_artsdata_ids_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'rdf'
+require 'linkeddata'
+require 'shacl'
+
+class ShaclTest < Minitest::Test
+  def setup
+    @shacl = SHACL.open('../../shacl/has_artsdata_ids_shacl.ttl')
+  end
+
+  def test_full_event_should_pass
+    graph = RDF::Graph.load("./fixtures/places_to_test_artsdata_ids.jsonld")
+    puts graph.dump(:jsonld)
+    report =  @shacl.execute(graph)  
+    
+    puts report
+   
+    assert report.conform?
+  end
+end
