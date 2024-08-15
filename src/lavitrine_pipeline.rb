83a596aab19e9df6031b631cca91ae833b29a84c
Gregory Saumier-Finch <gregory@culturecreates.com>
2024-08-15 09:44:01 -0400

    added smaller workflow

--- src/lavitrine_pipeline.rb
+++ src/lavitrine_pipeline.rb
@@ -3,7 +3,6 @@
 require_relative './utils/artsdata'
 
 def LavitrinePipeline(**args)
-  
   pipeline = ArtsdataPipeline.new
 
   artifact = ''
@@ -23,33 +22,33 @@ def LavitrinePipeline(**args)
   #### pipeline.load(file: "../output/raw-#{graph.split("/").last}.json" )
   # Custom transformations that justify needing this code rather than using solely the Artsdata API. 
   puts "#{Time.now}: Starting transforms..."
-   pipeline.transform("./sparql/remove_blank_literals.sparql")
-   puts "#{Time.now}: Starting transform transform_single_events..."
-   pipeline.transform("./sparql/transform_single_events.sparql")
-   puts "#{Time.now}: Starting transform transform_series_events..."
-   pipeline.transform("./sparql/transform_series_events.sparql")
-   puts "#{Time.now}: Starting small transforms..."
-   pipeline.transform("./sparql/remove_eventforindex.sparql")
-   pipeline.transform("./sparql/remove_temporary_eventtype.sparql")
-   pipeline.transform("./sparql/convert_image_object_to_url.sparql")
-   pipeline.transform("./sparql/convert_image_literal_to_uri.sparql")
-   pipeline.transform("./sparql/convert_url_literal_to_uri.sparql")
-   pipeline.transform("./sparql/convert_sameas_literal_to_uri.sparql")
-   puts "#{Time.now}: Starting transform convert_offers_to_aggregate_offer..."
-   pipeline.transform("./sparql/convert_offers_to_aggregate_offer.sparql")
-   puts "#{Time.now}: Starting transform remove_footlight_aggregate_offer..."
-   pipeline.transform("./sparql/remove_footlight_aggregate_offer.sparql")
-   puts "#{Time.now}: Starting transform fix_aggregate_offer_url..."
-   pipeline.transform("./sparql/fix_aggregate_offer_url.sparql")
-   puts "#{Time.now}: Starting transform fix_aggregate_offers_missing_url..."
-   pipeline.transform("./sparql/fix_aggregate_offers_missing_url.sparql")
-   puts "#{Time.now}: Starting transform remove_blank_literals..."
-   pipeline.transform("./sparql/remove_blank_literals.sparql")
-  
+  pipeline.transform("./sparql/remove_blank_literals.sparql")
+  puts "#{Time.now}: Starting transform transform_single_events..."
+  pipeline.transform("./sparql/transform_single_events.sparql")
+  puts "#{Time.now}: Starting transform transform_series_events..."
+  pipeline.transform("./sparql/transform_series_events.sparql")
+  puts "#{Time.now}: Starting small transforms..."
+  pipeline.transform("./sparql/remove_eventforindex.sparql")
+  pipeline.transform("./sparql/remove_temporary_eventtype.sparql")
+  pipeline.transform("./sparql/convert_image_object_to_url.sparql")
+  pipeline.transform("./sparql/convert_image_literal_to_uri.sparql")
+  pipeline.transform("./sparql/convert_url_literal_to_uri.sparql")
+  pipeline.transform("./sparql/convert_sameas_literal_to_uri.sparql")
+  puts "#{Time.now}: Starting transform convert_offers_to_aggregate_offer..."
+  pipeline.transform("./sparql/convert_offers_to_aggregate_offer.sparql")
+  puts "#{Time.now}: Starting transform remove_footlight_aggregate_offer..."
+  pipeline.transform("./sparql/remove_footlight_aggregate_offer.sparql")
+  puts "#{Time.now}: Starting transform fix_aggregate_offer_url..."
+  pipeline.transform("./sparql/fix_aggregate_offer_url.sparql")
+  puts "#{Time.now}: Starting transform fix_aggregate_offers_missing_url..."
+  pipeline.transform("./sparql/fix_aggregate_offers_missing_url.sparql")
+  puts "#{Time.now}: Starting transform remove_blank_literals..."
+  pipeline.transform("./sparql/remove_blank_literals.sparql")
 
-   pipeline.dump("../output/transformed-#{artifact}.json")
 
- ###### pipeline.load(file: "../output/transformed-#{graph.split("/").last}.json" )
+  pipeline.dump("../output/transformed-#{artifact}.json")
+
+  #### pipeline.load(file: "../output/transformed-#{graph.split("/").last}.json" )
 
   puts "#{Time.now}: Framing..."
   pipeline.frame( "../frame/lavitrine_event_frame.jsonld")
@@ -69,5 +68,15 @@ def LavitrinePipeline(**args)
   puts "#{Time.now}: Done with #{artifact}"
 end
 
+# Check if a parameter is provided
+if ARGV.length != 1
+  puts "Usage: ruby lavitrine_pipeline.rb <graph>"
+  exit
+end
 
+# Get the graph parameter
+graph = ARGV[0]
+
+# Call the LavitrinePipeline method with the graph parameter
+LavitrinePipeline(graph: graph)
 
