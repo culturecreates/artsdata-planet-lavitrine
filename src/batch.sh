4e8fc22052aafe9f3bf497637e1b7faecdd72ffb
Gregory Saumier-Finch <gregory@culturecreates.com>
2026-05-18 16:14:27 -0400

    switch derived-grandtheatre-qc-ca source to Github Workflow ETL instead of Huginn ETL.

--- src/batch.sh
+++ src/batch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Call the Ruby script with arguments
-bundle exec ruby lavitrine_pipeline.rb http://kg.artsdata.ca/culture-creates/huginn/derived-grandtheatre-qc-ca
+bundle exec ruby lavitrine_pipeline.rb http://kg.artsdata.ca/culture-creates/artsdata-planet-gtq/derived-grandtheatre-qc-ca
 bundle exec ruby lavitrine_pipeline.rb http://kg.artsdata.ca/culture-creates/footlight/placedesarts-com
 bundle exec ruby lavitrine_pipeline.rb http://kg.artsdata.ca/resource/dia-logGraphs
 bundle exec ruby lavitrine_pipeline.rb http://kg.artsdata.ca/culture-creates/footlight/hector-charland-com
